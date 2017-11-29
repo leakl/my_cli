4f4d1c838872ee1260480c23f04669b6881dc342
nick <leakl@qq.com>
2017-11-29 14:56:38 +0800

    更新URL

--- static/js/config.js
+++ static/js/config.js
@@ -1,11 +1,11 @@
  // 配置 Mock 路径
  require.config({
-    baseUrl: '/static/',
+    baseUrl: '/static/js/',
     paths: {
         vue: 'lib/Vue.v2.5.6',
         layer: 'lib/layer/layer',
         mock: 'lib/mock-min',
         dev: 'js/mock',
-        jq: 'lib/jquery.3.2.1.min'
+        jq: 'lib/jquery3.2.1.min'
     }
 })
